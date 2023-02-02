a754c111d28ce1f706632261fbb8ad3ec9215458
quang.trieu <quang.trieu@lifesup.com.vn>
2023-02-02 13:42:45 +0700

    Using Bootstrap in Angular |

--- src/app/Container/container.component.ts
+++ src/app/Container/container.component.ts
@@ -7,6 +7,7 @@ import { Component } from "@angular/core";
     <div>
       <h1>This is template property html</h1>
       <p class="para">Some text</p>
+      <p class="alert alert-warning">quang test boostrap</p>
     </div>
   `,
   styles: ["h1{color: red; background: blue; text-align: center }", ".para{color: blue; background: #000; margin-top: 20px }"]
