e35de49eeab69dd4cd64c6c37dab4514ecdb4a68
quang.trieu <quang.trieu@lifesup.com.vn>
2023-02-02 13:55:35 +0700

    Different Selectors in Angular

--- src/app/Container/container.component.ts
+++ src/app/Container/container.component.ts
@@ -1,17 +1,19 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-container',
-  // templateUrl: './container.component.html',
-  template: `
-    <div>
-      <h1>This is template property html</h1>
-      <p class="para">Some text</p>
-      <p class="alert alert-warning">quang test boostrap</p>
-    </div>
-  `,
-  styles: ["h1{color: red; background: blue; text-align: center }", ".para{color: blue; background: #000; margin-top: 20px }"]
-  // styleUrls: ['./container.component.css']
+  // selector: 'app-container',
+  // selector: '[app-container]',
+  selector: '.app-container',
+  templateUrl: './container.component.html',
+  // template: `
+  //   <div>
+  //     <h1>This is template property html</h1>
+  //     <p class="para">Some text</p>
+  //     <p class="alert alert-warning">quang test boostrap</p>
+  //   </div>
+  // `,
+  // styles: ["h1{color: red; background: blue; text-align: center }", ".para{color: blue; background: #000; margin-top: 20px }"]
+  styleUrls: ['./container.component.css']
 })
 export class ContainerComponent {
 
