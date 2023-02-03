95c907f707b1b8a66e9a483ce02ce9171c98d54c
quang.trieu <quang.trieu@lifesup.com.vn>
2023-02-03 16:27:20 +0700

    Binding Directive Properties

--- src/app/CustomDirective/host-binding.directive.ts
+++ src/app/CustomDirective/host-binding.directive.ts
@@ -1,20 +1,36 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHostBinding]'
 })
-export class HostBindingDirective {
+export class HostBindingDirective implements OnInit {
 
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
-  @HostBinding('style.backgroundColor') background: string = 'red';
+  // defaultColor: string = 'red';
+  // highlightColor: string = 'pink';
+
+  // @Input() defaultColor: string = 'red';
+  // @Input() highlightColor: string = 'pink';
+
+  @Input() defaultColor: string = 'red';
+  @Input('appHostBinding') highlightColor: string = 'pink';
+
+
+  ngOnInit() {
+    this.background = this.defaultColor;
+  }
+  // @HostBinding('style.backgroundColor') background: string = 'red';
+  @HostBinding('style.backgroundColor') background: string = this.defaultColor;
 
   @HostListener('mouseenter') mouseenter() {
-    this.background = 'pink';
+    // this.background = 'pink';
+    this.background = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.background = 'red';
+    // this.background = 'red';
+    this.background = this.defaultColor;
   }
 
 }
