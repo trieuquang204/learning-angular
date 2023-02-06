930626e3f07308247aafc651eb31e7fd21694447
quang.trieu <quang.trieu@lifesup.com.vn>
2023-02-06 16:52:00 +0700

    Understanding Operators of RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 
 
@@ -15,37 +16,45 @@ export class AppComponent implements OnInit {
 
   }
 
-  myObservable = new Observable((observer) => {
-    setTimeout(() => {
-      observer.next("1")
-    }, 1000);
+  // myObservable = new Observable((observer) => {
+  //   setTimeout(() => {
+  //     observer.next("1")
+  //   }, 1000);
 
-    setTimeout(() => {
-      observer.next("2")
-    }, 2000);
+  //   setTimeout(() => {
+  //     observer.next("2")
+  //   }, 2000);
 
-    // setTimeout(() => {
-    //   observer.error(new Error('Something wrong'))
-    // }, 2000);
+  //   // setTimeout(() => {
+  //   //   observer.error(new Error('Something wrong'))
+  //   // }, 2000);
 
-    setTimeout(() => {
-      observer.next("3")
-    }, 3000);
+  //   setTimeout(() => {
+  //     observer.next("3")
+  //   }, 3000);
 
-    setTimeout(() => {
-      observer.complete()
-    }, 4000);
+  //   setTimeout(() => {
+  //     observer.complete()
+  //   }, 4000);
 
-    // console.log('start')
-    // observer.next("1")
-    // observer.next("2")
-    // observer.next("3")
-  });
+  //   // console.log('start')
+  //   // observer.next("1")
+  //   // observer.next("2")
+  //   // observer.next("3")
+  // });
 
+  array1 = [1, 2, 6, 7, 8];
+  array2 = ['A', 'B', 'C'];
+
+  myObservable = from(this.array1)
+
+  transform = this.myObservable.pipe(map((val) => {
+    return val * 5
+  }))
 
 
   ngOnInit() {
-    this.myObservable.subscribe((val) => {
+    this.transform.subscribe((val) => {
       console.log('val', val)
     }, (error ) => {
       alert(error.message)
